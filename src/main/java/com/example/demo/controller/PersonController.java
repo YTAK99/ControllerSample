9ae5ce10d98544d10ce6ea5ae524fb7b4e2d7620
YTAK99 <ytzzang99@gmail.com>
2026-08-13 14:31:22 +0900

    initialize

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -4,15 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class PersonController {
 
     @GetMapping("/")
     public String index() {
-        return "index";
+        return "Person Controller";
     }
 
-    @GetMapping("/users")
-    public String users() {
-        return "/users";
+    @GetMapping("/user")
+    public String user() {
+        return "User Controller";
     }
 }
